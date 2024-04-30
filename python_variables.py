26c8a0ba18dc18274c408bd484969b867d9d7d7e
Prince Punshi <ppunshi@jhg.com.au>
2024-05-01 06:43:58 +1000

    adding python_variables file

---
+++ python_variables.py
@@ -0,0 +1,23 @@
+# Basic python variables assignment
+x=5
+y=10 
+z=3.5   ##float value
+
+a= int(z) ## converted float into int
+
+b=True ## boolean value
+type(a)  ## To find the data type of any value
+
+# Pyhton is case sensitive so doesn't recognise small true or false
+
+c= "Prince"  # String datatype
+
+# In python you dont need to explicit tell the datatype of a variable, complier judge the datatype itself
+# print is used to print something on terminal
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
